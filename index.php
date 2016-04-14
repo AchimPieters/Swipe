057e6dca3f40a24221dfbf1aaefdd5a4373e192c
Achim Pieters <achimpieters@hotmail.com>
2016-04-14 14:56:02 +0200

    Swipe

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        
+		<title>Simple jQuery slider</title>
+		
+		<!-- Include stylesheets -->
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="css/Font.css" rel="stylesheet" type="text/css">
+		
+
+		<!-- Get jQuery from CDN -->
+		<script src="scripts/jquery-1.10.1.min.js"></script>
+		<script src="scripts/jquery-migrate-1.2.1.min.js"></script>
+
+		<!-- Include the slider scripts -->
+		<script type="text/javascript" src="scripts/jquery.slider.js"></script>
+		<script type="text/javascript" src="scripts/backstretch.js"></script>
+      
+        <!-- Include the Settings scripts -->
+		<script type="text/javascript" src="scripts/Settings.js"></script>
+
+	</head>
+	<body>
+		
+        
+        <!-- START - Logo Area-->
+		<header id="top">
+			<div id="logo"><h2>Studio Pieters˙</h2></div>
+		</header>
+        <!-- START - Logo Area-->
+
+
+		<!-- START - Slide Area-->
+			<div class='pageblock' id='fullscreen'>
+				<div class='slider'>
+                    
+                    
+                    <!-- START - The First Slide -->
+					<div class='slide' id="first"> <!-- Change the ID to Firts, second, thirth, fourth......  as you add more you'll need to add it also to custom.js -->
+						<div class='slidecontent'>
+                            
+                            
+                            <!-- START - Add Your content code here-->
+							<p>Responsive Design and the slider works perfectly fine on mobile devices, tablets etc.</p> <!-- Add a heading text-->
+							<h1>Slide one.</h1> 
+                            <!-- Add Your content code here - STOP-->
+                            
+                            
+						</div>
+					</div>
+                    <!--The Second Slide - STOP -->
+                    
+                    <!-- START - The Second Slide -->
+					<div class='slide' id="second"> <!-- Change the ID to Firts, second, thirth, fourth......  as you add more you'll need to add it also to custom.js -->
+						<div class='slidecontent'>
+                            
+                            
+                            <!-- START - Add Your content code here-->
+							<p>You Need some Knowledge of PHP, HTML5 or CSS3.</p> <!-- Add a heading text-->
+							<h1>Slide two.</h1> 
+                            <!-- Add Your content code here - STOP-->
+                            
+                            
+						</div>
+					</div>
+                    <!--The Second Slide - STOP -->
+                    
+                    <!-- START - The Thirth Slide -->
+					<div class='slide' id="thirth"> <!-- Change the ID to Firts, second, thirth, fourth......  as you add more you'll need to add it also to custom.js -->
+						<div class='slidecontent'>
+                            
+                            
+                            <!-- START - Add Your content code here-->
+							<p>Creating a new slider is simple – add your image, add your slide and the script will do the rest.</p> <!-- Add a heading text-->
+							<h1>Slide Three.</h1> 
+                            <!-- Add Your content code here - STOP-->
+                            
+                            
+						</div>
+					</div>
+                    <!--The Thirth Slide - STOP -->
+                    
+                    
+                   
+				</div>
+			</div>
+        <!-- Slide Area - STOP -->
+	</body>
+</html>
